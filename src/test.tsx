61f5ad44fb4d6553dd7b24639fbb4c407ee0515f
Tim Wan <timwan@microsoft.com>
2017-07-27 10:51:08 -0700

    Subject

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {action, observable, computed} from "mobx";
+import {observer} from "mobx-react";
 
-class HelloWorld extends React.Component{
-    public render() {
-        return (<div> Hello World! </div>);
+
+class Info {
+    @observable private _subject:string;
+
+    @computed public get Subject():string {return this._subject;}
+
+    @action
+    public updateName(inSubject:string) : void {
+        this._subject = inSubject;
+    }
+}
+
+let myInfo : Info = new Info();
+
+Office.initialize = () => {
+    myInfo.updateName((Office.context.mailbox.item as Office.MessageRead).subject);
+}
+
+@observer
+class HelloWorld extends React.Component<{}, {}> {
+    public render(): JSX.Element {
+        return <div> This Message's Subject is: {myInfo.Subject}</div>;
     }
 }
 
