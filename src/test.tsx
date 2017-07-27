8b0750cfd81f1228182e57d98ffb1994f94e3fc9
Tim Wan <timwan@microsoft.com>
2017-07-27 16:24:14 -0700

    Working Array

--- src/test.tsx
+++ src/test.tsx
@@ -15,6 +15,30 @@ class Info {
     }
 }
 
+class Template {
+    @observable private _title:string;
+    @observable private _body:string;
+
+    constructor(inTitle:string, inBody:string) {
+        this.updateTitle(inTitle);
+        this.updateBody(inBody);
+    }
+
+    @computed public get Title():string {return this._title;}
+
+    @action updateTitle(inTitle:string) : void {
+        this._title = inTitle;
+    }
+
+    @action updateBody(inBody:string) : void {
+        this._body = inBody;
+    }
+}
+
+let rgTemplates = new Array<Template>(2);
+rgTemplates[0] = new Template("Hi Mom", "Foobar");
+rgTemplates[1] = new Template("I'll Be Late", "I am running late and can't make it");
+
 let myInfo : Info = new Info();
 
 Office.initialize = () => {
@@ -28,7 +52,44 @@ class HelloWorld extends React.Component<{}, {}> {
     }
 }
 
+export interface SquareButtonProps { value: string;}
+class SquareButton extends React.Component<SquareButtonProps, undefined > {
+    
+    render() {
+        return(
+            <button className="squareButton">{this.props.value}</button>
+        )
+    }
+}
+
+export interface ButtonBoardProps {buttons: Array<Template>}
+class ButtonBoard extends React.Component<ButtonBoardProps, undefined> {
+    render() {
+        /*
+        var components = this.props.buttons.map(function(button){
+            return (<SquareButton value={button.Title()} />);
+        })
+        */
+        return (
+            <div>{this.props.buttons.map(button  => <div><SquareButton value={button.Title} /></div>)}</div>
+        )
+        /*
+        return (
+                <div><SquareButton value="Hello" /><SquareButton value="hi mom" /></div>
+        )
+            */
+    }
+}
+
 ReactDOM.render(
-    (<HelloWorld />),
+    (<ButtonBoard buttons={rgTemplates} />),
+        document.getElementById("app")
+);
+
+
+/*
+ReactDOM.render(
+    (<SquareButton value="Robot Chicken Hello World"/>),
         document.getElementById("app")
         );
+*/
